e45b2b57053b96323a27c2bedf37008f1c7ee070
utk-dwd <161426380+utk-dwd@users.noreply.github.com>
2025-02-20 10:09:52 +0530

    Update Stripes.jsx

--- React_one/src/components/Stripes.jsx
+++ React_one/src/components/Stripes.jsx
@@ -4,12 +4,12 @@ import Stripe from './Stripe'
 function Stripes() {
 
     var data = [
-        {url: "src/assets/DogeBG.png", number: "$0.3381"},
-        {url: "src/assets/ChillguyBG.png", number: "$0.1641"},
-        {url: "src/assets/ShibaBG.png", number: "$0.00002258"},
-        {url: "src/assets/BrettBG.png", number: "$0.13"},
-        {url: "src/assets/PenguBG.png", number: "$0.036"},
-        {url: "src/assets/pepeBG.png", number: "$0.0000202"}
+        {url: "https://i.postimg.cc/MG3cgcds/DogeBG.png", number: "$0.3381"},
+        {url: "https://i.postimg.cc/MTccwWLz/Chillguy-BG.png", number: "$0.1641"},
+        {url: "https://i.postimg.cc/rwLwHGcy/ShibaBG.png", number: "$0.00002258"},
+        {url: "https://i.postimg.cc/bJKZN7Xk/BrettBG.png", number: "$0.13"},
+        {url: "https://i.postimg.cc/jjxdxsLG/PenguBG.png", number: "$0.036"},
+        {url: "https://i.postimg.cc/8PvCwK2n/pepeBG.png", number: "$0.0000202"}
     ]
 
   return (
