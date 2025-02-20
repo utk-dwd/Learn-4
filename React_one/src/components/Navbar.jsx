8a74a83b82170adf2157f73f8707d2dd62cd43a5
utk-dwd <161426380+utk-dwd@users.noreply.github.com>
2025-02-20 10:09:09 +0530

    Update Navbar.jsx

--- React_one/src/components/Navbar.jsx
+++ React_one/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div className='max-w-screen-xl mx-auto p-3 flex items-center py-6 justify-between border-b-[2px] border-zinc-700'>
         <div className='nleft flex'>
-          <img src="src/assets/Meme_Coins_Logo.png" alt='Logo' className='w-20' />
+          <img src="https://i.postimg.cc/44DcJ9Zq/Meme-Coins-Logo.png" alt='Logo' className='w-20' />
           <div className='links flex gap-14 ml-20'>
               {["Home", "About", "Docs", "", "Join"].map((elem, index) => (
                   elem.length === 0  ? <span key={index} className='w-[2px] h-7 bg-zinc-700'></span> : (<a key={index}  className='text-lg flex items-center gap-1' href='#'>
